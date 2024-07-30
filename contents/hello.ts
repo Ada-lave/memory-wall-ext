8b2f2d888b615711444f22ceb7c725c52f1242a2
Ada-lave <mega.kefi36@gmail.com>
2024-07-30 07:54:21 +0300

    refactor navigation handler logic to class logic

--- contents/hello.ts
+++
@@ -1,7 +0,0 @@
-export {}
-
-let currPath = document.location.href
-
-if (!currPath.includes("https://pamyat-naroda.ru/heroes/") && !currPath.includes("https://mlcity.ru")) {
-    document.location.href = "https://pamyat-naroda.ru/heroes/"
-}   
---
+++ utils/navigationHandler.ts
@@ -0,0 +1,10 @@
+class NavigationHandler {
+    init() {
+        let currPath = document.location.href
+        if (!currPath.includes("https://pamyat-naroda.ru/heroes/") && !currPath.includes("https://mlcity.ru")) {
+            document.location.href = "https://pamyat-naroda.ru/heroes/"
+        } 
+    }
+}  
+
+export default NavigationHandler
