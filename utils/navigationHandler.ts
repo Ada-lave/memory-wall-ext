1e381f28cd8b77bbe87f607a80b087d679fd9d02
Ada-lave <mega.kefi36@gmail.com>
2024-08-06 15:55:33 +0300

    add more avalible pages

--- utils/navigationHandler.ts
+++ utils/navigationHandler.ts
@@ -6,7 +6,13 @@ class NavigationHandler {
     async init() {
         let currPath = document.location.href
         let state = await this.redirectSetting.getState()
-        if (!currPath.includes("https://pamyat-naroda.ru/heroes/") && !currPath.includes("https://mlcity.ru") && state) {
+        if (
+            !currPath.includes("https://pamyat-naroda.ru/heroes/") && 
+            !currPath.includes("https://pamyat-naroda.ru/documents/") && 
+            !currPath.includes("https://pamyat-naroda.ru/warunit/") &&
+            !currPath.includes("https://mlcity.ru") && 
+            state
+        ) {
             document.location.href = "https://pamyat-naroda.ru/heroes/"
         } 
     }
